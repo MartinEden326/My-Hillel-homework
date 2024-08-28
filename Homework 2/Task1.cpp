255053c109edce813ec84a01147f8243744d2f7d
MartinEden326 <lunarfog326@gmail.com>
2024-08-28 14:50:24 +0300

    Add files via upload

---
+++ Homework 2/Task1.cpp
@@ -0,0 +1,8 @@
+//Напишіть програму, яка відображає повідомлення I love C++.
+#include <iostream>
+
+int main()
+{
+    std::cout << "\nI love C++\n";
+    return 0;
+}
---
+++ Homework 2/Task3.cpp
@@ -0,0 +1,11 @@
+/* Баги: введіть цю програму та скомпілюйте її.
+   Чому вона дає збій? Як це можна виправити? */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //Ошибка в строке 7, отсутствует " и ;
+  cout << Is there a bug here?"
+  return 0;
+}    
