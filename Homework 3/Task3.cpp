d8ec69fb9938c3f02fecb9864684630774344479
MartinEden326 <lunarfog326@gmail.com>
2024-09-02 07:41:00 +0300

    Add files via upload

---
+++ Homework 3/Task3.cpp
@@ -0,0 +1,9 @@
+// Оголосіть константу для числа рі, що дорівнює 3.14159.
+#include <iostream>
+
+int main()
+{
+    const float pi = 3.14159;
+    std::cout << pi;
+    return 0;
+}
---
+++ Homework 3/Task4.cpp
@@ -0,0 +1,10 @@
+// Оголосіть змінну типу float та ініціалізуйте її, використовуючи константу рі.
+#include <iostream>
+
+int main()
+{
+    const float pi = 3.14159;
+    float number = pi;
+    std::cout << number;
+    return 0;
+}
